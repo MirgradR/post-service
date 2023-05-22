59b18674da51ab8cbcbf9c006b1a056230664d4f
Ruslan K <ruslan.kuyanets@yandex.ru>
2023-05-22 20:39:49 +0300

    fix

--- migrations/1684777155549-create_post.ts
+++ migrations/1684777155549-create_post.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class  %npmConfigName%Migration1684773951388 implements MigrationInterface {
-    name = ' %npmConfigName%Migration1684773951388'
+export class CreatePost1684777155549 implements MigrationInterface {
+    name = 'CreatePost1684777155549'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "posts" ("id" character varying NOT NULL, "title" character varying NOT NULL, "message" character varying NOT NULL, "author_id" character varying NOT NULL, "is_published" boolean NOT NULL, "created_at" character varying NOT NULL, "updated_at" character varying NOT NULL, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
