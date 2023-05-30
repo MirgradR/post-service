ad349f9d1b51af32b8b0da08fc5e4e77e4c397ee
Ruslan K <ruslan.kuyanets@yandex.ru>
2023-05-30 16:35:40 +0300

    add pagination dto

---
+++ libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
@@ -0,0 +1,23 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { PostAgregate } from '@lib/post/domain';
+import { PostRepository } from '@lib/post/providers';
+import { Logger } from '@nestjs/common';
+import { GetPostsQuery } from './get-posts.query';
+
+@QueryHandler(GetPostsQuery)
+export class GetPostQueryHandler
+  implements IQueryHandler<GetPostsQuery, [[PostAgregate], number]>
+{
+  private readonly logger = new Logger(GetPostQueryHandler.name);
+
+  constructor(private readonly postRepository: PostRepository) {}
+
+  async execute(query: GetPostsQuery): Promise<[[PostAgregate], number]> {
+    const [data, count] = await this.postRepository.findAll().catch((err) => {
+      this.logger.error(err);
+      return [[], 0];
+    });
+
+    return [data, count] as [[PostAgregate], number];
+  }
+}
